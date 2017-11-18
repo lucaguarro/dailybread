44ef51410c0a3368086b704da1aa09d5a03d72e7
Luca Guarro <luca.guarro@gmail.com>
2017-11-18 11:51:46 -0800

    created angular components for routing page

---
+++ src/app/featured-sellers-container/featured-sellers-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-sellers-container',
+  templateUrl: './featured-sellers-container.component.html',
+  styleUrls: ['./featured-sellers-container.component.css']
+})
+export class FeaturedSellersContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
