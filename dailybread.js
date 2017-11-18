0eb6d2e479cb1e47c7d65a49e821287a2872cbb0
JoshLorenzen <joshlorenzen49@gmail.com>
2017-11-18 13:46:33 -0800

    Can access our remote database in a very insecure manner

--- dailybread.js
+++ dailybread.js
@@ -3,12 +3,28 @@ const path = require('path');
 const app = express();
 
 
+var mysql      = require('mysql');
+var conn = mysql.createConnection({
+  host     : '138.68.227.178',
+  user     : 'root',
+  password : 'root',
+  database : 'dailybread'
+});
+
+
 app.get('/api/', (req, res) => 
 {
-    let {id, user} = req.params;
-    res.write("Your id is " + id + ".<br>");
-    res.end("Your user is " + user + ".")
-
+    //conn.connect()
+    conn.query('SELECT * from sellers', function(err, rows, fields) {
+        if (!err)
+        {
+            console.log('The solution is: ', rows);
+            res.send(rows);
+        }
+        else
+          console.log(err);
+      });
+      //conn.end();
 });
 
 app.use(express.static(path.join(__dirname, 'dist')));
