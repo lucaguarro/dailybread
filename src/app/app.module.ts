72f57a0ee13e799ae6535677e04d51fa952df791
Luca Guarro <luca.guarro@gmail.com>
2017-11-18 12:07:59 -0800

    template for nav bar is up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeaturedDealsContainerComponent } from './featured-deals-container/featured-deals-container.component';
+import { DealComponent } from './featured-deals-container/deal/deal.component';
+import { FeaturedSellersContainerComponent } from './featured-sellers-container/featured-sellers-container.component';
+import { SellerComponent } from './featured-sellers-container/seller/seller.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FeaturedDealsContainerComponent,
+    DealComponent,
+    FeaturedSellersContainerComponent,
+    SellerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/featured-deals-container/featured-deals-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-deals-container',
+  templateUrl: './featured-deals-container.component.html',
+  styleUrls: ['./featured-deals-container.component.css']
+})
+export class FeaturedDealsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
